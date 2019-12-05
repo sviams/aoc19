1b9f6f666b869d47cf5b054a4c3255b8dc7ea27f
Anders Jändel <anders.jandel@factor10.com>
2019-12-05 12:00:29 +0100

    Day 5

---
+++ src/test/kotlin/Day5Spec.kt
@@ -0,0 +1,23 @@
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertEquals
+
+object Day5Spec: Spek({
+
+    given("AoC 19.5.1") {
+
+        it("works with task data") {
+            assertEquals(6745903, Day5.solvePt1(readLines("5.txt").first().split(",").map { it.toInt() }))
+        }
+    }
+
+    given("AoC 19.5.2") {
+
+        it("works with task data") {
+            assertEquals(9168267, Day5.solvePt2(readLines("5.txt").first().split(",").map { it.toInt() }))
+        }
+
+    }
+})
