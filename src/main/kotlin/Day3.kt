682d7eee265222440e48b352161944611817ba64
Anders Jändel <anders.jandel@factor10.com>
2019-12-06 15:40:59 +0100

    Day 3 polish

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -1,9 +1,9 @@
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
-data class Pos(val x: Int, val y: Int, val steps: Int) {
-    fun move(other: Pos) = Pos(x + other.x, y + other.y, other.steps)
-    fun times(other: Pos) = Pos(x * other.x, y * other.y, steps)
+data class Pos(val x: Int, val y: Int) {
+    fun move(other: Pos) = Pos(x + other.x, y + other.y)
+    fun times(other: Pos) = Pos(x * other.x, y * other.y)
     fun distanceTo(other: Pos) = Math.abs(x - other.x) + Math.abs(y - other.y)
     override fun equals(other: Any?): Boolean {
         val o = other as Pos
@@ -15,42 +15,43 @@ typealias Path = List<Pos>
 
 object Day3 {
 
-    val CENTRAL = Pos(0,0, 0)
-    val DOWN = Pos(0, -1, 0)
-    val UP = Pos(0, 1, 0)
-    val LEFT = Pos(-1, 0, 0)
-    val RIGHT = Pos(1, 0, 0)
+    data class Result(val pos: Pos, val steps: Int)
+
+    val CENTRAL = Pos(0,0)
+
+    val xMap = mapOf('D' to 0, 'U' to 0, 'R' to 1, 'L' to -1 )
+    val yMap = mapOf('D' to -1, 'U' to 1, 'R' to 0, 'L' to 0 )
 
     fun pathToPoints(input: String) : List<Pos> =
-        input.split(",").fold(listOf(Pos(0,0, 0))) { acc, move ->
-            val delta: Pos = when (move.first()) {
-                'D' -> DOWN
-                'U' -> UP
-                'R' -> RIGHT
-                else -> LEFT
-            }
+        input.split(",").fold(listOf(Pos(0,0))) { acc, move ->
+            val dir = move.first()
+            val dX = xMap[dir] ?: 0
+            val dY = yMap[dir] ?: 0
             val dist = move.substring(1).toInt()
             (1 .. dist).fold(acc) { dAcc, dDist ->
-                val totalDelta = Pos(delta.x * dDist, delta.y * dDist, dAcc.last().steps + 1)
+                val totalDelta = Pos(dX * dDist, dY * dDist)
                 dAcc.plus(acc.last().move(totalDelta))
             }
         }
 
-    fun intersections(paths: List<Path>) : ImmutableList<Pos> {
+    fun intersections(paths: List<Path>) : ImmutableList<Result> {
         val one = paths.first()
         val other = paths.last()
-        return one.fold(emptyList<Pos>()) { acc, pos ->
+        return one.foldIndexed(emptyList<Result>()) { index, acc, pos ->
             if (other.contains(pos)) {
-                val fromOther = other.get(other.indexOf(pos))
-                (acc + Pos(pos.x, pos.y, pos.steps + fromOther.steps))
+                (acc + Result(Pos(pos.x, pos.y), index + other.indexOf(pos)))
             }
             else acc
         }.toImmutableList()
     }
 
-    fun solvePt1(input: List<String>) : Int = intersections(input.map { pathToPoints(it) }).map { it.distanceTo(CENTRAL) }.sorted()[1]
+    fun solvePt1(input: List<String>) : Int = intersections(input.map { pathToPoints(it) }).map { it.pos.distanceTo(CENTRAL) }.sorted()[1]
 
-    fun solvePt2(input: List<String>) : Int = intersections(input.map { pathToPoints(it) }).map { it.steps }.sorted()[1]
+    fun solvePt2(input: List<String>) : Int {
+        val asd = intersections(input.map { pathToPoints(it) })
+        val wer = asd.map { it.steps }.sorted()
+        return wer[1]
+    }
 
 
 }
