2d58adeb3528df006d65ec8bfb12052b6869d0ee
Anders Jändel <anders.jandel@factor10.com>
2019-12-09 22:07:28 +0100

    Refactored IntCodeComputer into mutable and immutable versions

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,21 +1,25 @@
 object Day9 {
 
-    data class IntCodeComputer(val state: MutableMap<Long, Long>, val output: MutableList<Long>, val input: MutableList<Long>, var lastOp: Long, var lastPos: Long, val lastOut: Long, var relBase: Long) {
-
+    object IntCodeComputer {
         fun mode(chars: List<Char>, index: Int): Char = chars.drop(index).firstOrNull() ?: '0'
-
-        fun twoParams(modeChars: List<Char>, pos: Long): List<Long> =
-            listOf(valueOf(mode(modeChars, 0), state[pos+1]!!), valueOf(mode(modeChars, 1), state[pos+2]!!))
-
-        fun valueOf(modeChar: Char, dest: Long): Long = when (modeChar) {
+        fun twoParams(modeChars: List<Char>, pos: Long, state: Map<Long, Long>, relBase: Long): List<Long> =
+            listOf(valueOf(state, mode(modeChars, 0), state[pos+1]!!, relBase), valueOf(state, mode(modeChars, 1), state[pos+2]!!, relBase))
+        fun valueOf(state: Map<Long, Long>, modeChar: Char, dest: Long, relBase: Long): Long = when (modeChar) {
             '2' -> state[dest + relBase] ?: 0
             '1' -> dest
             else -> state[dest] ?: 0
         }
+        fun mutable(program: Map<Long, Long>, input: List<Long>) =
+            MutableIntCodeComputer(program.toMutableMap(), emptyList<Long>().toMutableList(), input.toMutableList(), 0L, 0L, 0L, 0L)
 
-        tailrec fun run(pos: Long): IntCodeComputer {
-            val asd = state[pos]!!
-            val op = asd % 100
+        fun immutable(program: Map<Long, Long>, input: List<Long>) =
+            ImmutableIntCodeComputer(program, emptyList(), input, 0L, 0L, 0L)
+    }
+
+    data class MutableIntCodeComputer(val state: MutableMap<Long, Long>, val output: MutableList<Long>, val input: MutableList<Long>, var lastOp: Long, var lastPos: Long, val lastOut: Long, var relBase: Long) {
+
+        tailrec fun run(pos: Long): MutableIntCodeComputer {
+            val op = state[pos]!! % 100
             if (op == 99L) {
                 lastOp = 99
                 return this
@@ -25,7 +29,7 @@ object Day9 {
 
             val params = when (op) {
                 3L,4L,9L -> emptyList()
-                else -> twoParams(modeChars, pos)
+                else -> IntCodeComputer.twoParams(modeChars, pos, state, relBase)
             }
 
             val destModeChar = if (modeChars.drop(params.size).isNotEmpty()) modeChars.last() else '0'
@@ -41,12 +45,12 @@ object Day9 {
                 1L -> state[dest] = params[0] + params[1]
                 2L -> state[dest] = params[0] * params[1]
                 3L -> state[dest] = if (input.size > 0 ) input.removeAt(0) else return this
-                4L -> output.add(valueOf(destModeChar, lastParam))
+                4L -> output.add(IntCodeComputer.valueOf(state, destModeChar, lastParam, relBase))
                 7L -> if (params[0] < params[1]) state[dest] = 1 else state[dest] = 0
                 8L -> if (params[0] == params[1]) state[dest] = 1 else state[dest] = 0
             }
 
-            if (op == 9L) relBase += valueOf(destModeChar, lastParam)
+            if (op == 9L) relBase += IntCodeComputer.valueOf(state, destModeChar, lastParam, relBase)
 
             val nextPos = when (op) {
                 3L,4L,9L -> pos+2
@@ -57,13 +61,73 @@ object Day9 {
 
             return run(nextPos)
         }
-
-
     }
 
-    fun solve(program: List<Long>, input: List<Long>) : Long {
+    data class ImmutableIntCodeComputer(val state: Map<Long, Long>, val output: List<Long>, val input: List<Long>, val lastOp: Long, val relBase: Long, val pos: Long) {
+
+        companion object {
+            tailrec fun run(cpu: ImmutableIntCodeComputer): ImmutableIntCodeComputer {
+                val next = cpu.execute()
+                return if (next.lastOp == 99L) next else run(next)
+            }
+        }
+
+        fun execute(): ImmutableIntCodeComputer {
+            val op = state[pos]!! % 100
+            if (op == 99L) return ImmutableIntCodeComputer(state, output, input, op, relBase, pos)
+
+            val modeChars = state[pos].toString().toCharArray().dropLast(2).reversed()
+
+            val params = when (op) {
+                3L,4L,9L -> emptyList()
+                else -> IntCodeComputer.twoParams(modeChars, pos, state, relBase)
+            }
+
+            val destModeChar = if (modeChars.drop(params.size).isNotEmpty()) modeChars.last() else '0'
+            val lastParam = state[pos+1+params.size] ?: 0L
+            val dest: Long = when (destModeChar) {
+                '2' -> lastParam + relBase
+                else -> lastParam
+            }
+
+            val nextLastOp = op
+
+            if (op == 3L && input.isEmpty()) return this
+
+
+            val nextState: Map<Long, Long> = when (op) {
+                1L -> state + (dest to params[0] + params[1])
+                2L -> state + (dest to params[0] * params[1])
+                3L -> state + (dest to input[0])
+                7L -> if (params[0] < params[1]) state + (dest to 1L) else state + (dest to 0L)
+                8L -> if (params[0] == params[1]) state + (dest to 1L) else state + (dest to 0L)
+                else -> state
+            }
+
+            val nextInput = if (op == 3L) input.drop(1) else input
+            val nextOutput = if (op == 4L) output + IntCodeComputer.valueOf(state, destModeChar, lastParam, relBase) else output
+            val nextRelBase = if (op == 9L) relBase + IntCodeComputer.valueOf(state, destModeChar, lastParam, relBase) else relBase
+
+            val nextPos = when (op) {
+                3L,4L,9L -> pos+2
+                5L -> if (params[0] != 0L) params[1] else pos+3
+                6L -> if (params[0] == 0L) params[1] else pos+3
+                else -> pos+4
+            }
+            return ImmutableIntCodeComputer(nextState, nextOutput, nextInput, nextLastOp, nextRelBase, nextPos)
+        }
+    }
+
+    fun solveImmutable(program: List<Long>, input: List<Long>) : Long {
+        val startState = program.foldIndexed(emptyMap<Long, Long>()) { index, acc, i ->  acc.plus(index.toLong() to i)}
+        val startCpu = IntCodeComputer.immutable(startState, input)
+        val endCpu = ImmutableIntCodeComputer.run(startCpu)
+        return endCpu.output.last()
+    }
+
+    fun solveMutable(program: List<Long>, input: List<Long>) : Long {
         val startState = program.foldIndexed(emptyMap<Long, Long>()) { index, acc, i ->  acc.plus(index.toLong() to i)}.toMutableMap()
-        return IntCodeComputer(startState, emptyList<Long>().toMutableList(), input.toMutableList(), 0L, 0L, 0L, 0L).run(0).output.last()
+        return IntCodeComputer.mutable(startState, input.toMutableList()).run(0).output.last()
     }
 
 }
--- src/test/kotlin/Day9Spec.kt
+++ src/test/kotlin/Day9Spec.kt
@@ -9,30 +9,38 @@ object Day9Spec: Spek({
     given("AoC 19.9.1") {
 
         it("works with test data") {
-            assertEquals(99, Day9.solve(listOf(109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99), listOf(1L)))
+            assertEquals(99, Day9.solveMutable(listOf(109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99), listOf(1L)))
         }
 
         it("works with test data 2") {
-            assertEquals(1219070632396864, Day9.solve(listOf(1102,34915192,34915192,7,4,7,99,0), listOf(1L)))
+            assertEquals(1219070632396864, Day9.solveMutable(listOf(1102,34915192,34915192,7,4,7,99,0), listOf(1L)))
         }
 
         it("works with test data 3") {
-            assertEquals(1125899906842624L, Day9.solve(listOf(104,1125899906842624,99), listOf(1L)))
+            assertEquals(1125899906842624L, Day9.solveMutable(listOf(104,1125899906842624,99), listOf(1L)))
         }
 
         it("works with Day 5 task data") {
-            assertEquals(6745903, Day9.solve(readLines("5.txt").first().split(",").map { it.toLong() }, listOf(1L)))
+            assertEquals(6745903, Day9.solveMutable(readLines("5.txt").first().split(",").map { it.toLong() }, listOf(1L)))
         }
 
-        it("works with task data") {
-            assertEquals(2465411646, Day9.solve(readLines("9.txt").first().split(",").map { it.toLong() }, listOf(1L)))
+        it("works with task data mutable") {
+            assertEquals(2465411646, Day9.solveMutable(readLines("9.txt").first().split(",").map { it.toLong() }, listOf(1L)))
+        }
+
+        it("works with task data immutable") {
+            assertEquals(2465411646, Day9.solveImmutable(readLines("9.txt").first().split(",").map { it.toLong() }, listOf(1L)))
         }
     }
 
     given("AoC 19.9.2") {
 
-        it("works with task data") {
-            assertEquals(69781, Day9.solve(readLines("9.txt").first().split(",").map { it.toLong() }, listOf(2L)))
+        it("works with task data mutable") {
+            assertEquals(69781, Day9.solveMutable(readLines("9.txt").first().split(",").map { it.toLong() }, listOf(2L)))
+        }
+
+        it("works with task data immutable") {
+            assertEquals(69781, Day9.solveImmutable(readLines("9.txt").first().split(",").map { it.toLong() }, listOf(2L)))
         }
 
     }
