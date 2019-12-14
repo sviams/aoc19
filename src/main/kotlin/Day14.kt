26044063f64d9ed3d0853969cfd03def3511131e
Anders Jändel <anders.jandel@factor10.com>
2019-12-14 20:11:18 +0100

    Day 14 refactored

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,5 +1,3 @@
-import kotlin.math.max
-
 object Day14 {
 
     data class Ingredient(val name: String, val quantity: Long) {
@@ -17,40 +15,41 @@ object Day14 {
         return Recipe(inputs, Ingredient(outName, outQuantity.toLong()))
     }
 
-    tailrec fun reverseUntil(recipes: List<Recipe>, required: List<Ingredient>, stock: Map<String, Long>) : List<Ingredient> {
+    tailrec fun oreRequiredFor(recipes: List<Recipe>, required: List<Ingredient>, stock: Map<String, Long>) : List<Ingredient> {
         if (required.all { it.isOre() }) return required
-
         val target = required.filter { !it.isOre() }.first()
         val alreadyHave = stock[target.name] ?: 0
         val requiredAfterStock = if (target.quantity - alreadyHave < 0) 0 else target.quantity - alreadyHave
         val stockAmountLeft = if (alreadyHave - target.quantity < 0) 0 else alreadyHave - target.quantity
-
         val recipe = recipes.first { it.output.name == target.name }
         val outQuantity = recipe.output.quantity
         val requiredMultiples = if (requiredAfterStock > 0)
             requiredAfterStock / outQuantity + if (requiredAfterStock % outQuantity == 0L) 0 else 1
         else 0
-
         val newStock = stock.plus(target.name to stockAmountLeft + requiredMultiples*outQuantity-requiredAfterStock)
-
         val needed = if (requiredMultiples > 0) recipe.input.map { Ingredient(it.name, it.quantity * requiredMultiples) } else emptyList()
         val nextRequired = required.minus(target).plus(needed)
-
-        return reverseUntil(recipes, nextRequired, newStock)
+        return oreRequiredFor(recipes, nextRequired, newStock)
     }
 
-    tailrec fun binFindMax(recipes: List<Recipe>, current: Long, upper: Long) : Long {
-        val atCurrent = reverseUntil(recipes, listOf(Ingredient("FUEL", current)), emptyMap()).fold(0L) {acc, p -> acc + p.quantity}
-        val atUpper = reverseUntil(recipes, listOf(Ingredient("FUEL", upper)), emptyMap()).fold(0L) {acc, p -> acc + p.quantity}
-        val boundedUpper = if (atUpper > 1000000000000L) 0 else atUpper
-        val diff = upper - current
-        val nextCurrent = if (boundedUpper > atCurrent) current + diff/2 else current
-        val nextUpper = if (boundedUpper > atCurrent) upper*2 else upper - diff/2
-        if (nextCurrent == nextUpper-1) return nextCurrent+1
-        return binFindMax(recipes, nextCurrent, nextUpper)
+    fun oreRequiredForFuel(recipes: List<Recipe>, fuelAmount: Long) : Long =
+        oreRequiredFor(recipes, listOf(Ingredient("FUEL", fuelAmount)), emptyMap()).fold(0L) { acc, p -> acc + p.quantity}
+
+    tailrec fun bisectMax(range: LongRange, limit: Long, knownAtLower: Long = -1, knownAtUpper: Long = -1, fn: (Long) -> Long) : Long {
+        val atLower = if (knownAtLower == -1L) fn(range.first) else knownAtLower
+        val atUpper = if (knownAtUpper == -1L) fn(range.last) else knownAtUpper
+        val boundedUpper = if (atUpper > limit) 0 else atUpper
+        val rangeDiff = range.last - range.first
+        val nextLower = if (boundedUpper > atLower) range.first + rangeDiff else range.first
+        val nextUpper = if (boundedUpper > atLower) range.last*2 else range.last - rangeDiff/2
+        val nextKnownLower = if (nextLower == range.first) atLower else if (nextLower == range.last) atUpper else -1L
+        val nextKnownUpper = if (nextUpper == range.first) atLower else if (nextUpper == range.last) atUpper else -1L
+        if (nextLower == nextUpper-1) return nextLower
+        return bisectMax((nextLower .. nextUpper), limit, nextKnownLower, nextKnownUpper, fn)
     }
 
-    fun solvePt1(input: List<String>) : Long = reverseUntil(input.map(::parseRecipe), listOf(Ingredient("FUEL", 1)), emptyMap()).fold(0L) {acc, p -> acc + p.quantity}
+    fun solvePt1(input: List<String>) : Long = oreRequiredForFuel(input.map(::parseRecipe), 1)
 
-    fun solvePt2(input: List<String>) : Long = binFindMax(input.map(::parseRecipe), 1,1000000000000L)
+    fun solvePt2(input: List<String>) : Long =
+        bisectMax((1 .. 1000000000000L), 1000000000000L) { oreRequiredForFuel(input.map(::parseRecipe), it)}
 }
